4d746f5bf04b9817fe47ff632a080559eea065a5
ValentinaSuarez0324 <126030347+ValentinaSuarez0324@users.noreply.github.com>
2023-04-14 19:17:15 -0500

    Feature hexadecimal (#1)
    * programa conversor decimal a octal
    
    * programa conversor decimal a octal
    
    ---------
    
    Co-authored-by: root <root@LAPTOP-U0PN02F1.localdomain>

---
+++ pgm_hexadecimal_to_decimal/src/App/app.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class app{
+    public static void main(String[] args) {
+        Scanner Capturar=new Scanner(System.in);
+        operaciones operaciones = new operaciones();
+        int num1;
+        int num2;
+        int resultado;
+        System.out.println("Bienvenido al programa de sumas");
+        System.out.println("############## MENU ###############");
+        System.out.println("Ingresa un número");
+
+        num1 = Capturar.nextInt();
+        System.out.println("Ingresa un segundo número");
+        num2 = Capturar.nextInt();
+        //resultado = num1 + num2;
+
+        System.out.println("El resultado es " + operaciones.Suma(num1, num2) );
+
+
+
+ }
+public static int Suma (int dato1, int dato2) {
+    int resultado = dato1+dato2; 
+    return resultado;
+}
+
+}           
